5d53649453394449de6e6fb87b809aa54d560ce5
Andrew <an6reww@gmail.com>
2026-07-21 20:26:35 +0000

    Demo let%profile

---
+++ ppx/ppx_profile.mli
@@ -0,0 +1,10 @@
+(** PPX rewriter providing the [%profile] extension. *)
+
+open Ppxlib
+
+val wrap_body : loc:location -> name:string -> expression -> expression
+
+val expand :
+  ctxt:Expansion_context.Extension.t -> structure_item -> structure_item
+
+val extension : Extension.t
